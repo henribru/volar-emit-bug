b8f57dba3b2878ced5f5a5e2aea3d3168f99a844
henribru <6639509+henribru@users.noreply.github.com>
2021-10-07 23:25:30 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld :msg="'Hello Vue 3 + TypeScript + Vite'" @foo-bar="(foo) => console.log(foo)" @foo-baz="(foo) => console.log(foo)" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent(
+  {
+    components: {
+      HelloWorld
+    },
+    setup() {
+      return { console }
+    }
+  }
+)
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface Foo {
+    foo: string;
+}
+  
+export interface Bar {
+    bar: number;
+}
